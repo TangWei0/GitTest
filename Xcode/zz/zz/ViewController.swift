f4b5871b92d8676f928a5b12e040f08141fa4f2f
itou1 <toui860722@yahoo.co.jp>
2015-12-15 23:05:34 +0900

    Button文本框以及插入图片变更

--- Xcode/zz/zz/ViewController.swift
+++ Xcode/zz/zz/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var ImageBool = false
+    @IBOutlet weak var ImageButton: UIButton!
     @IBOutlet var iv: UIImageView!
     @IBAction func pictureViewButton(sender: AnyObject) {
-        iv.image = UIImage(named : "mouse.jpg")
+        if !ImageBool {
+            ImageBool = true
+            iv.image = UIImage(named : "mouse.jpg")
+            ImageButton.setTitle("不显示图片", forState: UIControlState.Normal)
+        }
+        else {
+            ImageBool = false
+            ImageButton.setTitle("显示图片", forState: UIControlState.Normal)
+            iv.image = UIImage(named : "")
+        }
+        
     }
     
     override func viewDidLoad() {
