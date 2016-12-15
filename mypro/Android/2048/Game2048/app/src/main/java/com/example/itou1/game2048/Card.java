98cfe9841dd9ab26e926817a3a324a4e90283956
itou1 <itou1@gw.nsw.co.jp>
2016-12-15 17:49:35 +0900

    2016/12/15 18:00

--- mypro/Android/2048/Game2048/app/src/main/java/com/example/itou1/game2048/Card.java
+++ mypro/Android/2048/Game2048/app/src/main/java/com/example/itou1/game2048/Card.java
@@ -1,17 +1,25 @@
 package com.example.itou1.game2048;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class Card extends FrameLayout {
+
+    private int number = 0;
+    private TextView tvNumber;
+
     public Card (Context context) {
         super(context);
 
         tvNumber = new TextView(getContext());
         tvNumber.setTextSize(32);
+        tvNumber.setBackgroundColor(0x33FFFFFF);
+        tvNumber.setGravity(Gravity.CENTER);
 
         LayoutParams lp = new LayoutParams(-1,-1);
+        lp.setMargins(10, 10, 0, 0);
         addView(tvNumber,lp);
 
         setNumber(0);
@@ -24,25 +32,15 @@ public class Card extends FrameLayout {
     public void setNumber(int number) {
         this.number = number;
 
-        tvNumber.setText(number + "");
+        if (number > 0) {
+            tvNumber.setText(number + "");
+        } else {
+            tvNumber.setText("");
+        }
     }
 
     public boolean equals(Card o){
         return getNumber() == o.getNumber();
     }
 
-    private int number = 0;
-    private TextView tvNumber;
-
-    @Override
-    protected  void  onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w,h,oldw,oldh);
-
-        int cardSize = (Math.min(w,h) - 10) / 4;
-        addCards(cardSize);
-    }
-
-    private void addCards(int cardSize) {
-        Card card;
-    }
 }
