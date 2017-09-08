f8fa05a7fe91e90e5a59f3a31659f88598a8d2e7
汤伟 <toui860722@gmail.com>
2017-09-08 22:31:04 +0900

    漏掉的

---
+++ mypro/C#/财务管理/MoneyManage/MoneyManage/MoneyManage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyManage
+{
+    public partial class MoneyManage : Form
+    {
+        public MoneyManage()
+        {
+            InitializeComponent();
+            FixedValue FixedValue = new FixedValue();
+
+            //MoneyManageCalculate m = new MoneyManageCalculate();
+
+            DateTime date = System.DateTime.Now;
+            MoneyManageLabel.Text = date.Year + "年度" + MoneyManageLabel.Text;
+
+            MoneyManageDataGridView.ColumnCount = FixedValue.MONEYMANAGEVIEWCOLUMN;
+            MoneyManageDataGridView.RowCount = FixedValue.MONTHS;
+
+            for (int i = 0; i < FixedValue.MONTHS; i++)
+            {
+                MoneyManageDataGridView.Rows[i].Cells[0].Value = i + 1;
+            }
+
+            //MoneyManageDataGridView[0, 0].Value = 1;
+            //MoneyManageDataGridView.Rows[0].Cells[0].Value = 1;
+            //MoneyManageDataGridView.Rows[1].Cells[0].Value = 2;
+            //MoneyManageDataGridView.Rows[2].Cells[0].Value = 3;
+
+            System.Console.WriteLine(date.Month);
+            //System.Console.WriteLine(m.cal(100000).ToString());
+        }
+
+    }
+}
