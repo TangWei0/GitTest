30edeb35881a6f18c71666c005620af5a185c074
itou1 <NSW00_905181@nsw.local>
2017-09-12 20:14:08 +0900

    list排序

--- Win_Home/toui/JAVA/SwingApp/src/jp/co/doraxdora/form/DisplayCard.java
+++ Win_Home/toui/JAVA/SwingApp/src/jp/co/doraxdora/form/DisplayCard.java
@@ -1,13 +1,37 @@
 package jp.co.doraxdora.form;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class DisplayCard 
 {
-	int num =(int)(Math.random() * 3) + 1;
-	int cardNumber = 1;
-
+	ArrayList<Integer> cardList = new ArrayList<Integer>();
+	ArrayList<Integer> cardDefault = new ArrayList<Integer>();
+	Calculation Calculation = new Calculation();
+	
+	int num =(int)(Math.random() * 20) + 1;
+	
+	public ArrayList<Integer> CardDefault()
+	{
+		cardDefault.clear();
+		for (int i=1; i<=54; i++)
+		{
+			cardDefault.add(i);
+		}
+		return cardDefault;
+	}
+	
+	public ArrayList<Integer> SelectCard()
+	{
+		CardDefault();
+		for (int i=1; i<=num; i++)
+		{
+			int selectIndex = (int)(Math.random() * cardDefault.size());
+			cardList.add(Calculation.Search(cardList, cardDefault.get(selectIndex)),cardDefault.get(selectIndex));
+			cardDefault.remove(selectIndex);
+		}		
+		return cardList;
+	}
 		
 	public String CardDisplay(int i)
 	{
