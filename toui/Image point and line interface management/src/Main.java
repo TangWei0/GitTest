4618402150fbd4656cd63c1d40158b4269c161d9
itou1 <NSW00_905181@nsw.local>
2017-11-08 14:48:15 +0900

    课题：图片点csv界面化005

--- toui/Image point and line interface management/src/Main.java
+++ toui/Image point and line interface management/src/Main.java
@@ -1,10 +1,8 @@
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -24,14 +22,24 @@ public class Main extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	static CSVReadWrite CSVReadWrite = new CSVReadWrite();
+	static ShortestPath ShortestPath = new ShortestPath();
+
 	public static ArrayList<String> PointList = new ArrayList<String>();
 	public static ArrayList<ArrayList<String>> EdgeList = new ArrayList<ArrayList<String>>();
 	public static ArrayList<String> ImagePathList = new ArrayList<String>();
 
-	static CSVReadWrite CSVReadWrite = new CSVReadWrite();
-	static ShortestPath ShortestPath = new ShortestPath();
+	// 画面コントロール
+	public static JPanel panel = new JPanel();
+	public static JLabel PointLabel = new JLabel("頂点を選んでください");
+	public static JButton ShortestPathButton = new JButton("最短経路計算");
+	public static JComboBox<String> PointComboBox = new JComboBox<String>();
+	public static JRadioButton EditPointRadioButton = new JRadioButton("頂点編集");
+	public static JRadioButton AddPointRadioButton = new JRadioButton("頂点追加");
+	public static ButtonGroup PointRadioButtonGroup = new ButtonGroup();
 
-	static ButtonGroup group = new ButtonGroup();
+	public static String OperationType = "頂点編集";
+
 	/**
 	 * Launch the application.
 	 */
@@ -62,61 +70,90 @@ public class Main extends JFrame {
 		setResizable(false);
 		setVisible(true);
 
-		JPanel panel = new JPanel();
 		Container contentPane = getContentPane();
 		contentPane.add(panel);
 		panel.setLayout(null);
 
-		//最短経路計算ボタン
-		JButton ShortestPathButton = new JButton("最短経路計算");
-		panel.add(ShortestPathButton);
+		// 最短経路計算ボタン
 		ShortestPathButton.setBounds(600, 500, 150, 50);
 		ShortestPathButton.setForeground(Color.blue);
+		panel.add(ShortestPathButton);
 
-		ShortestPathButton.addActionListener(new StartButtonListener());
+		ShortestPathButton.addActionListener(new ShortestPathButtonListener());
 
 		/*
-		String combodata[] = new String[PointList.size() + 2];
-		combodata[0] = "頂点を選んでください";
-		for (int i = 0; i < PointList.size(); i++) {		
-			combodata[i+1] = PointList.get(i);
-		}
-		combodata[PointList.size()+1] = "追加頂点";
-		*/
+		 * String combodata[] = new String[PointList.size() + 2]; combodata[0] =
+		 * "鬆らせ繧帝∈繧薙〒縺上□縺輔＞"; for (int i = 0; i < PointList.size(); i++) {
+		 * combodata[i+1] = PointList.get(i); } combodata[PointList.size()+1] =
+		 * "霑ｽ蜉�鬆らせ";
+		 */
+
+		// 頂点操作ラジオボタン
+		EditPointRadioButton.setSelected(true);
+		EditPointRadioButton.setBounds(100, 50, 100, 30);
+		AddPointRadioButton.setBounds(200, 50, 100, 30);
+		PointRadioButtonGroup.add(EditPointRadioButton);
+		PointRadioButtonGroup.add(AddPointRadioButton);
+		panel.add(EditPointRadioButton);
+		panel.add(AddPointRadioButton);
+
+		EditPointRadioButton.addActionListener(new OperationTypeButtonListener());
+		AddPointRadioButton.addActionListener(new OperationTypeButtonListener());
+
+		//頂点comboBox
+		PointComboBoxUpdate(); 
 		
-		//
-		JRadioButton EditRadio = new JRadioButton("編集頂点");
-		EditRadio.setBounds(100, 50, 100,30);
-		JRadioButton AddRadio = new JRadioButton("追加頂点");
-		AddRadio.setBounds(200, 50, 100,30);
-		
-		
-		group.add(EditRadio);
-		group.add(AddRadio);
-		panel.add(EditRadio);
-		panel.add(AddRadio);
-		
-		group.setOnCheckedChangeListener(new onCheckedChanged());
-		
-		Vector<String> combodata = new Vector<String>(PointList);
-		//頂点ComboBox
-		JComboBox<String> PointCombo = new JComboBox<String>(combodata);
-		PointCombo.setBounds(100, 100, 200, 30);
-		panel.add(PointCombo);
-		
-		//頂点Label
-		JLabel PointLabel = new JLabel("頂点を選んでください");
-		PointLabel.setBounds(100, 70, 200,30);
+		// 頂点Label
+		PointLabel.setBounds(100, 70, 200, 30);
 		panel.add(PointLabel);
+		PointLabel.setVisible(true);
+	}
+
+	//頂点comboBoxの値更新
+	public void PointComboBoxUpdate() {
+		Vector<String> combodata = new Vector<String>(PointList);
+		PointComboBox = new JComboBox<String>(combodata);
+		PointComboBox.setBounds(100, 100, 200, 30);
+		panel.add(PointComboBox);
+		PointComboBox.setVisible(true);
+
+		PointComboBox.addActionListener(new boxListener());
 	}
 	
-	public class onCheckedChanged implements ActionListener {  
-        // TODO Auto-generated method stub  
-        RadioButton tempButton = (RadioButton)findViewById(checkedId); // 通过RadioGroup的findViewById方法，找到ID为checkedID的RadioButton  
-        // 以下就可以对这个RadioButton进行处理了  
-    }  
+	// 頂点comboBoxイベント
+ 	public class boxListener implements ActionListener {
 
-	public class StartButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String s = (String) PointComboBox.getSelectedItem();
+			System.out.println(s);
+		}
+
+	}
+
+	// 頂点操作ラジオボタンイベント
+	public class OperationTypeButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (OperationType != e.getActionCommand()) {
+				if (e.getActionCommand() == "頂点追加") {
+					PointComboBox.setVisible(false);
+					PointLabel.setVisible(false);
+				} else {
+					PointComboBoxUpdate();
+					PointLabel.setVisible(true);
+				}
+				OperationType = e.getActionCommand();
+				System.out.println(OperationType);
+			}
+		}
+	}
+
+	// 最短経路計算ボタンイベント
+	public class ShortestPathButtonListener implements ActionListener {
+
+		@Override
 		public void actionPerformed(ActionEvent e) {
 			long start = System.currentTimeMillis();
 			ShortestPath.shortestPath();
