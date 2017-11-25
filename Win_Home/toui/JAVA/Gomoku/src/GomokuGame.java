a4face2efba4865f595d2135d3048770285f7e54
汤伟 <toui860722@gmail.com>
2017-11-25 23:35:29 +0900

    数独破解器项目_新建

--- Win_Home/toui/JAVA/Gomoku/src/GomokuGame.java
+++ Win_Home/toui/JAVA/Gomoku/src/GomokuGame.java
@@ -1,14 +1,10 @@
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
---
+++ Win_Home/toui/JAVA/Sudoku/src/SudokuGame.java
@@ -0,0 +1,108 @@
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SudokuGame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static int WIDTH_SIZE = 800;
+	private static int HEIGHT_SIZE = 800;
+	
+	// 画面コントロール
+	static JPanel panel = new JPanel();
+	static JLabel PointLabel = new JLabel("緊急な事故を発生した頂点を選んでください");
+	static JLabel PointInforLabel = new JLabel("立ち入り禁止頂点：");
+	static JLabel MessageLabel = new JLabel("最新最短経路ではないので、最短経路計算ボタンをクッリクしてください。");
+	static JButton ConfirmButton = new JButton("確定");
+	static JButton ShortestPathButton = new JButton("最短経路計算");
+	static JButton RebackButton = new JButton("復帰");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new SudokuGame();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	SudokuGame() {
+		setTitle("数独");
+		setSize(WIDTH_SIZE, HEIGHT_SIZE);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		Container contentPane = getContentPane();
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		JButton button1 = new JButton("button1");
+	    JButton button2 = new JButton("button2");
+	    JButton button3 = new JButton("button3");
+	    JButton button4 = new JButton("button4");
+	    JButton button5 = new JButton("button5");
+	    JButton button6 = new JButton("button6");
+	    JButton button7 = new JButton("button7");
+	    JButton button8 = new JButton("button8");
+	    JButton button9 = new JButton("button9");
+	    button1.setBounds(50, 50, 50, 50);
+	    button2.setBounds(50, 100, 50, 50);
+	    button3.setBounds(50, 150, 50, 50);
+	    button4.setBounds(50, 200, 50, 50);
+	    button5.setBounds(50, 250, 50, 50);
+	    button6.setBounds(50, 300, 50, 50);
+	    button7.setBounds(50, 350, 50, 50);
+	    button8.setBounds(50, 400, 50, 50);
+	    button9.setBounds(50, 450, 50, 50);
+	    //JButton button2 = new JButton("button2");
+	   // JButton button3 = new JButton("button3");
+	    //JButton button4 = new JButton("button4");
+
+
+
+		GridLayout layout = new GridLayout(9, 1);
+		panel.setLayout(layout);
+
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		panel.add(button7);
+		panel.add(button8);
+		panel.add(button9);
+
+	}
+
+}
