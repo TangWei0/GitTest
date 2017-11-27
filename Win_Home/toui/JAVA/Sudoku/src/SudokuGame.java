d3457c54c57215416c2ff472807c586790a3dd3c
汤伟 <toui860722@gmail.com>
2017-11-27 23:14:22 +0900

    数独破解器项目_画面设计
    舍去美观中直线，只显示9x9框

--- Win_Home/toui/JAVA/Sudoku/src/SudokuGame.java
+++ Win_Home/toui/JAVA/Sudoku/src/SudokuGame.java
@@ -1,21 +1,11 @@
 import java.awt.BasicStroke;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SudokuGame extends JFrame {
@@ -24,18 +14,14 @@ public class SudokuGame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private static int WIDTH_SIZE = 800;
 	private static int HEIGHT_SIZE = 800;
-	
+	private static int NUM_SIZE = 9;
+
 	// 画面コントロール
 	static JPanel panel = new JPanel();
-	static JLabel PointLabel = new JLabel("緊急な事故を発生した頂点を選んでください");
-	static JLabel PointInforLabel = new JLabel("立ち入り禁止頂点：");
-	static JLabel MessageLabel = new JLabel("最新最短経路ではないので、最短経路計算ボタンをクッリクしてください。");
-	static JButton ConfirmButton = new JButton("確定");
-	static JButton ShortestPathButton = new JButton("最短経路計算");
-	static JButton RebackButton = new JButton("復帰");
+	static JButton[][] sudoku = new JButton[NUM_SIZE][NUM_SIZE];
 
 	/**
 	 * Launch the application.
@@ -65,33 +51,42 @@ public class SudokuGame extends JFrame {
 		Container contentPane = getContentPane();
 		contentPane.add(panel);
 		panel.setLayout(null);
+		panel.setOpaque(false);
 
 		JButton button1 = new JButton("button1");
-	    JButton button2 = new JButton("button2");
-	    JButton button3 = new JButton("button3");
-	    JButton button4 = new JButton("button4");
-	    JButton button5 = new JButton("button5");
-	    JButton button6 = new JButton("button6");
-	    JButton button7 = new JButton("button7");
-	    JButton button8 = new JButton("button8");
-	    JButton button9 = new JButton("button9");
-	    button1.setBounds(50, 50, 50, 50);
-	    button2.setBounds(50, 100, 50, 50);
-	    button3.setBounds(50, 150, 50, 50);
-	    button4.setBounds(50, 200, 50, 50);
-	    button5.setBounds(50, 250, 50, 50);
-	    button6.setBounds(50, 300, 50, 50);
-	    button7.setBounds(50, 350, 50, 50);
-	    button8.setBounds(50, 400, 50, 50);
-	    button9.setBounds(50, 450, 50, 50);
-	    //JButton button2 = new JButton("button2");
-	   // JButton button3 = new JButton("button3");
-	    //JButton button4 = new JButton("button4");
+		JButton button2 = new JButton("button2");
+		JButton button3 = new JButton("button3");
+		JButton button4 = new JButton("button4");
+		JButton button5 = new JButton("button5");
+		JButton button6 = new JButton("button6");
+		JButton button7 = new JButton("button7");
+		JButton button8 = new JButton("button8");
+		JButton button9 = new JButton("button9");
+		button1.setBounds(50, 50, 50, 50);
+		button2.setBounds(50, 100, 50, 50);
+		button3.setBounds(50, 150, 50, 50);
+		button4.setBounds(50, 200, 50, 50);
+		button5.setBounds(50, 250, 50, 50);
+		button6.setBounds(50, 300, 50, 50);
+		button7.setBounds(50, 350, 50, 50);
+		button8.setBounds(50, 400, 50, 50);
+		button9.setBounds(50, 450, 50, 50);
+		// JButton button2 = new JButton("button2");
+		// JButton button3 = new JButton("button3");
+		// JButton button4 = new JButton("button4");
 
+		// GridLayout layout = new GridLayout(9, 1);
+		// panel.setLayout(null);
 
-
-		GridLayout layout = new GridLayout(9, 1);
-		panel.setLayout(layout);
+		button1.setContentAreaFilled(false);
+		button2.setContentAreaFilled(false);
+		button3.setContentAreaFilled(false);
+		button4.setContentAreaFilled(false);
+		button5.setContentAreaFilled(false);
+		button6.setContentAreaFilled(false);
+		button7.setContentAreaFilled(false);
+		button8.setContentAreaFilled(false);
+		button9.setContentAreaFilled(false);
 
 		panel.add(button1);
 		panel.add(button2);
@@ -103,6 +98,16 @@ public class SudokuGame extends JFrame {
 		panel.add(button8);
 		panel.add(button9);
 
+		panel.repaint();
+
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(3.0f));
+		g2.drawLine(50, 50, 50, 500);
 	}
 
 }
