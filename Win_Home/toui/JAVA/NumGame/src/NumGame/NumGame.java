c60d83d262096b8c56b253e3fd235f8ac97dfa18
itou1 <toui860722@yahoo.co.jp>
2017-12-11 22:01:14 +0900

    提高数独运算效率

--- Win_Home/toui/JAVA/NumGame/src/NumGame/NumGame.java
+++ Win_Home/toui/JAVA/NumGame/src/NumGame/NumGame.java
@@ -1,4 +1,4 @@
-package sudoku;
+package NumGame;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,35 +10,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-public class sudoku {
+public class NumGame {
 
+	public static String ReadFile = "Input/Lev15.csv";
+	public static String WirteFile = "Output/Lev15.csv";
 	public static ArrayList<ArrayList<Integer>> Candidate = new ArrayList<ArrayList<Integer>>();
 
-	public static ArrayList<Integer> Item = new ArrayList<Integer>();
-	public static ArrayList<Integer> def = new ArrayList<Integer>();
+	public static ArrayList<Integer> TmpItem = new ArrayList<Integer>();
+	public static ArrayList<Integer> Def = new ArrayList<Integer>();
+	public static ArrayList<Integer> Object = new ArrayList<Integer>();
 
 	public static int NUM_SIZE = 9;
 	public static int OBJECT_SIZE = 3;
 	public static int IMPOSSIBLE_SIZE = 20;
 
-	public static int count = 0;
-
-	public static int[][] SudokuNum = new int[NUM_SIZE][NUM_SIZE];
+	public static int a = 0;
 	public static int[] Impossible = new int[IMPOSSIBLE_SIZE];
 
 	public static void main(String[] args) {
 		for (int i = 0; i < NUM_SIZE; i++) {
-			def.add(i + 1);
+			Def.add(i + 1);
 		}
+		sudoku();
+	}
+	
+	public static void sudoku()
+	{
 		read();
-		while (count < NUM_SIZE * NUM_SIZE) {
+		while (Object.size() > 0) {
 			Investigation();
 		}
 		Write();
 	}
 
 	public static void read() {
-		File ReadCSV = new File("Input/Lev14.csv");
+		File ReadCSV = new File(ReadFile);
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(ReadCSV));
@@ -47,24 +53,26 @@ public class sudoku {
 			e.printStackTrace();
 		}
 		String PointLine;
-
+		String PointValue;
 		try {
 			while ((PointLine = br.readLine()) != null) {
-				String[] PointData = PointLine.split(",", 0);
-
-				for (int i = 0; i < PointData.length; i++) {
-					int value = Integer.parseInt(PointData[i]);
-					if (value == 0) {
-						Item = new ArrayList<Integer>(def);
-						Candidate.add(Item);
+				for (int i = 0; i < PointLine.length(); i++) {
+					if (i != PointLine.length() - 1) {
+						PointValue = PointLine.substring(i, i + 1);
 					} else {
-						Item = new ArrayList<Integer>();
-						Item.add(value);
-						Candidate.add(Item);
-						count++;
+						PointValue = PointLine.substring(i);
+					}
+					int value = Integer.parseInt(PointValue);
+					if (value == 0) {
+						TmpItem = new ArrayList<Integer>(Def);
+						Candidate.add(TmpItem);
+						Object.add(Candidate.size() - 1);
+					} else {
+						TmpItem = new ArrayList<Integer>();
+						TmpItem.add(value);
+						Candidate.add(TmpItem);
 					}
 				}
-
 			}
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
@@ -76,27 +84,22 @@ public class sudoku {
 	}
 
 	public static void Write() {
-		File file = new File("Output/Lev14.csv");
+		File file = new File(WirteFile);
 		try {
 			// 出力先を作成する
 			FileWriter fw = new FileWriter(file, false);
 			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-
 			// 内容を指定する
 			for (int i = 0; i < NUM_SIZE * NUM_SIZE; i++) {
-				if (i != 0 && i%NUM_SIZE == 0)
-				{
+				if (i != 0 && i % NUM_SIZE == 0) {
 					pw.println();
 				}
 				pw.print(Candidate.get(i).get(0));
-				pw.print(",");
 			}
 			// ファイルに書き出す
 			pw.close();
-
 			// 終了メッセージを画面に出力する
 			System.out.println("出力が完了しました。");
-
 		} catch (IOException ex) {
 			// 例外時処理
 			ex.printStackTrace();
@@ -104,27 +107,25 @@ public class sudoku {
 	}
 
 	public static void Investigation() {
-		for (int i = 0; i < NUM_SIZE * NUM_SIZE; i++) {
-			if (Candidate.get(i).size() == 1) {
-				continue;
-			}
-			Item = new ArrayList<Integer>();
-			Item = Candidate.get(i);
-			RangeCheck(i);
+		for (int i = 0; i < Object.size(); i++) {
+			int Index = Object.get(i);
+			TmpItem = new ArrayList<Integer>();
+			TmpItem = Candidate.get(Index);
+			RangeCheck(Index);
 			for (int CompareIndex : Impossible) {
 				if (Candidate.get(CompareIndex).size() != 1) {
 					continue;
 				}
-				int CompareFindIndex = Item.indexOf(Candidate.get(CompareIndex).get(0));
+				int CompareFindIndex = TmpItem.indexOf(Candidate.get(CompareIndex).get(0));
 				if (CompareFindIndex == -1) {
 					continue;
 				}
-				Item.remove(CompareFindIndex);
-				if (Item.size() == 1) {
-					count++;
+				TmpItem.remove(CompareFindIndex);
+				if (TmpItem.size() == 1) {
+					Object.remove(Object.indexOf(Index));
 				}
 			}
-			Candidate.set(i, Item);
+			Candidate.set(Index, TmpItem);
 		}
 	}
 
@@ -132,7 +133,6 @@ public class sudoku {
 		int X = i / NUM_SIZE;
 		int Y = i % NUM_SIZE;
 		int ImpossibleCount = 0;
-
 		// 横轴对象
 		for (int j = 0; j < NUM_SIZE; j++) {
 			if (j == Y) {
@@ -141,7 +141,6 @@ public class sudoku {
 			Impossible[ImpossibleCount] = XYToIndex(X, j);
 			ImpossibleCount++;
 		}
-
 		// 纵轴对象
 		for (int j = 0; j < NUM_SIZE; j++) {
 			if (j == X) {
@@ -150,11 +149,9 @@ public class sudoku {
 			Impossible[ImpossibleCount] = XYToIndex(j, Y);
 			ImpossibleCount++;
 		}
-
 		// 局部对象
 		int partInitX = X - X % OBJECT_SIZE;
 		int partInitY = Y - Y % OBJECT_SIZE;
-
 		for (int j = 0; j < NUM_SIZE; j++) {
 			int partIndexX = partInitX + j / OBJECT_SIZE;
 			int partIndexY = partInitY + j % OBJECT_SIZE;
