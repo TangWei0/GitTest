8c24b985986763de9ec60352a0d3487a35a83851
TangWei0 <toui860722@gmail.com>
2018-05-08 18:04:25 +0900

    增加问题卡初始化程序（随机抽取6张卡片）

--- TAGIRON/src/tgrCardStructure.java
+++ TAGIRON/src/tgrCardStructure.java
@@ -3,47 +3,35 @@ import java.util.Random;
 
 public class tgrCardStructure {
 
-	// 定数を定義する
-	static int DIGITALPARAMETERS = 2;
-	static int DIGITALSIZE = 20;
-	static int DIGITALHALFSIZE = 10;
-	static int SELECTDIGITALSIZE = 5;
-	static int QUESTIONSIZE = 21;
-	static int SELECTQUESTIONSIZE = 6;
-
-	// 抽選数字カードを定義する
-	public int[][] User1DigitalCardArray = new int[SELECTDIGITALSIZE][DIGITALPARAMETERS];
-	public int[][] User2DigitalCardArray = new int[SELECTDIGITALSIZE][DIGITALPARAMETERS];
-
 	public void tgrSelectDigitalCard() {
 		Random rand = new Random();
-		Integer[] user1SelectedIndex = new Integer[SELECTDIGITALSIZE];
-		Integer[] user2SelectedIndex = new Integer[SELECTDIGITALSIZE];
+		Integer[] user1SelectedIndex = new Integer[tgrMain.SELECTDIGITALSIZE];
+		Integer[] user2SelectedIndex = new Integer[tgrMain.SELECTDIGITALSIZE];
 		int num1, num2;
-		for (int i = 0; i < SELECTDIGITALSIZE; i++) {
+		for (int i = 0; i < tgrMain.SELECTDIGITALSIZE; i++) {
 			do {
-				num1 = rand.nextInt(DIGITALSIZE);
-				num2 = rand.nextInt(DIGITALSIZE);
+				num1 = rand.nextInt(tgrMain.DIGITALSIZE);
+				num2 = rand.nextInt(tgrMain.DIGITALSIZE);
 			} while ((Arrays.asList(user1SelectedIndex).contains(num1)) ||
 						(Arrays.asList(user1SelectedIndex).contains(num2)) ||
 						(Arrays.asList(user2SelectedIndex).contains(num1)) ||
 						(Arrays.asList(user2SelectedIndex).contains(num2)) ||
 						(num1 == num2));
-			tgrSelectedCardSort(user1SelectedIndex, num1);
-			tgrSelectedCardSort(user2SelectedIndex, num2);
+			tgrSelectedDigitalCardSort(user1SelectedIndex, num1);
+			tgrSelectedDigitalCardSort(user2SelectedIndex, num2);
 		}
-		User1DigitalCardArray = tgrDigitalCardDefind(user1SelectedIndex);
-		User2DigitalCardArray = tgrDigitalCardDefind(user2SelectedIndex);
+		tgrMain.User1DigitalCardArray = tgrDigitalCardDefind(user1SelectedIndex);
+		tgrMain.User2DigitalCardArray = tgrDigitalCardDefind(user2SelectedIndex);
 	}
 
-	private int[][] tgrDigitalCardDefind(Integer[] selectedIndex) {
-		int[][] digitalCardArrayTmp = new int[SELECTDIGITALSIZE][DIGITALPARAMETERS];
-		for (int i = 0; i < SELECTDIGITALSIZE; i++) {
-			digitalCardArrayTmp[i][0] = selectedIndex[i] % DIGITALHALFSIZE;
-			if (selectedIndex[i] % DIGITALHALFSIZE == 5) {
+	private Integer[][] tgrDigitalCardDefind(Integer[] selectedIndex) {
+		Integer[][] digitalCardArrayTmp = new Integer[tgrMain.SELECTDIGITALSIZE][tgrMain.DIGITALPARAMETERS];
+		for (int i = 0; i < tgrMain.SELECTDIGITALSIZE; i++) {
+			digitalCardArrayTmp[i][0] = selectedIndex[i] % tgrMain.DIGITALHALFSIZE;
+			if (selectedIndex[i] % tgrMain.DIGITALHALFSIZE == 5) {
 				digitalCardArrayTmp[i][1] = tgrMain.ColorEnum.valueOfByName("Green").getId();
 			} else {
-				if (selectedIndex[i] < DIGITALHALFSIZE) {
+				if (selectedIndex[i] < tgrMain.DIGITALHALFSIZE) {
 					digitalCardArrayTmp[i][1] = tgrMain.ColorEnum.valueOfByName("Red").getId();
 				} else {
 					digitalCardArrayTmp[i][1] = tgrMain.ColorEnum.valueOfByName("Blue").getId();
@@ -53,15 +41,15 @@ public class tgrCardStructure {
 		return digitalCardArrayTmp;
 	}
 
-	private void tgrSelectedCardSort(Integer[] selectedIndex, int num) {
-		for (int i = 0; i < SELECTDIGITALSIZE; i++) {
+	private void tgrSelectedDigitalCardSort(Integer[] selectedIndex, int num) {
+		for (int i = 0; i < tgrMain.SELECTDIGITALSIZE; i++) {
 			if (selectedIndex[i] == null) {
 				selectedIndex[i] = num;
 				return;
 			} else {
-				if ((num % DIGITALHALFSIZE < selectedIndex[i] % DIGITALHALFSIZE) ||
-						((num % DIGITALHALFSIZE == selectedIndex[i] % DIGITALHALFSIZE) && (num < selectedIndex[i]))) {
-					for (int j = SELECTDIGITALSIZE - 1; j > i; j--) {
+				if ((num % tgrMain.DIGITALHALFSIZE < selectedIndex[i] % tgrMain.DIGITALHALFSIZE) ||
+						((num % tgrMain.DIGITALHALFSIZE == selectedIndex[i] % tgrMain.DIGITALHALFSIZE) && (num < selectedIndex[i]))) {
+					for (int j = tgrMain.SELECTDIGITALSIZE - 1; j > i; j--) {
 						selectedIndex[j] = selectedIndex[j - 1];
 					}
 					selectedIndex[i] = num;
@@ -70,6 +58,31 @@ public class tgrCardStructure {
 					continue;
 				}
 			}
+		}
+	}
+
+	public void tgrSelectQuestionCard() {
+		int count = 0;
+		for (Integer num : tgrMain.usingQuestionCardArray) {
+			if (num != null) {
+
+			} else {
+				count++;
+				if (count == tgrMain.SELECTQUESTIONSIZE) {
+					tgrSelectQuestionCardInitialization();
+				}
+			}
+		}
+	}
+
+	private void tgrSelectQuestionCardInitialization() {
+		Random rand = new Random();
+		int num;
+		for (int i = 0; i < tgrMain.SELECTQUESTIONSIZE; i++) {
+			do {
+				num = rand.nextInt(tgrMain.QUESTIONSIZE);
+			} while (Arrays.asList(tgrMain.usingQuestionCardArray).contains(num));
+			tgrMain.usingQuestionCardArray[i] = num;
 		}
 	}
 }
--- TAGIRON/src/tgrMain.java
+++ TAGIRON/src/tgrMain.java
@@ -39,6 +39,22 @@ public class tgrMain {
 		}
 	}
 
+	// 定数を定義する
+	static int DIGITALPARAMETERS = 2;
+	static int DIGITALSIZE = 20;
+	static int DIGITALHALFSIZE = 10;
+	static int SELECTDIGITALSIZE = 5;
+	static int QUESTIONSIZE = 21;
+	static int SELECTQUESTIONSIZE = 6;
+
+	// 抽選数字カードを定義する
+	public static Integer[][] User1DigitalCardArray = new Integer[SELECTDIGITALSIZE][DIGITALPARAMETERS];
+	public static Integer[][] User2DigitalCardArray = new Integer[SELECTDIGITALSIZE][DIGITALPARAMETERS];
+
+	// 問題カードを定義する
+	public static Integer[] usingQuestionCardArray = new Integer[SELECTQUESTIONSIZE];
+	public static Integer[] usedQuestionCardArray = new Integer[QUESTIONSIZE];
+
 	static tgrCardStructure cardStructure = new tgrCardStructure();
 
 	/**
@@ -51,21 +67,104 @@ public class tgrMain {
 
 		// 計測したい処理を記述
 		cardStructure.tgrSelectDigitalCard();
-		
+		cardStructure.tgrSelectQuestionCard();
+
 		System.out.println("User1のカード：");
 		for (int j = 0; j < 5; j++) {
-			System.out.println(cardStructure.User1DigitalCardArray[j][0] + "," +
-					cardStructure.User1DigitalCardArray[j][1] + "→" +
-					ColorEnum.valueOf(cardStructure.User1DigitalCardArray[j][1]));
+			if (j == 4) {
+				System.out.println("[" + User1DigitalCardArray[j][0] + "," + ColorEnum.valueOf(User1DigitalCardArray[j][1]) + "]");
+			} else {
+				System.out.print("[" + User1DigitalCardArray[j][0] + "," + ColorEnum.valueOf(User1DigitalCardArray[j][1]) + "],");
+			}
 		}
+
 		System.out.println("User2のカード：");
 		for (int k = 0; k < 5; k++) {
-			System.out.println(cardStructure.User2DigitalCardArray[k][0] + "," +
-					cardStructure.User2DigitalCardArray[k][1] + "→" +
-					ColorEnum.valueOf(cardStructure.User2DigitalCardArray[k][1]));
+			if (k == 4) {
+				System.out.println("[" + User2DigitalCardArray[k][0] + "," + ColorEnum.valueOf(User2DigitalCardArray[k][1]) + "]");
+			} else {
+				System.out.print("[" + User2DigitalCardArray[k][0] + "," + ColorEnum.valueOf(User2DigitalCardArray[k][1]) + "],");
+			}
+		}
+
+		System.out.println("問題カード：");
+		for (int i = 0; i < 6; i++) {
+			System.out.println("問題カード番号" + usingQuestionCardArray[i] + ":" + tgrQuestionShow(usingQuestionCardArray[i]));
 		}
 
 		long end = System.currentTimeMillis();
-		System.out.println("処理時間：" + (end - start) + "ms");		
+		System.out.println("処理時間：" + (end - start) + "ms");
+	}
+
+	public static String tgrQuestionShow(int num) {
+		String title = "";
+		switch (num) {
+		case 0:
+			title = "8または9はどこ？";
+			break;
+		case 1:
+			title = "1または2はどこ？";
+			break;
+		case 2:
+			title = "連番になっているタイルはどこ？";
+			break;
+		case 3:
+			title = "青の数字タイルは何枚ある？";
+			break;
+		case 4:
+			title = "偶数は何枚ある？";
+			break;
+		case 5:
+			title = "赤の数字タイルは何枚ある？";
+			break;
+		case 6:
+			title = "赤の数の合計数は？";
+			break;
+		case 7:
+			title = "青の数の合計数は？";
+			break;
+		case 8:
+			title = "5枚のタイルすべての合計数は？";
+			break;
+		case 9:
+			title = "3または4はどこ？";
+			break;
+		case 10:
+			title = "同じ数字タイルのペアは何組ある？";
+			break;
+		case 11:
+			title = "中央の数字タイルの5以上？4以下？";
+			break;
+		case 12:
+			title = "奇数は何枚ある？";
+			break;
+		case 13:
+			title = "0はどこ？";
+			break;
+		case 14:
+			title = "数字タイルの最大の数から最小の数を引いた数は？";
+			break;
+		case 15:
+			title = "連続してとなり合っている色はどこ？";
+			break;
+		case 16:
+			title = "小さいほうから3枚の合計数は？";
+			break;
+		case 17:
+			title = "6または7はどこ？";
+			break;
+		case 18:
+			title = "中央の3枚の合計数は？";
+			break;
+		case 19:
+			title = "5はどこ？";
+			break;
+		case 20:
+			title = "大きいほうから3枚の合計数は？";
+			break;
+		default:
+			break;
+		}
+		return title;
 	}
 }
