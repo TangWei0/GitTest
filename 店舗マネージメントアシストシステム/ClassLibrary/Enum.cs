1354d3271156025c5e21f4014382be733cae6d43
TangWei0 <toui860722@gmail.com>
2019-12-30 17:02:36 +0900

    32ビットMD5ハッシュ値計算処理
    32ビットMD5ハッシュ値計算処理　チェックインした。
    ➝文字列から32ビットMD5ハッシュ値に変更する

---
+++ 店舗マネージメントアシストシステム/ClassLibrary/Enum.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary
+{
+    public class Enum
+    {
+        // 出力ハッシュ値用パラメータ(大文字か小文字を指定)
+        public enum E_HASH_PARAMETER
+        {
+            UPPER,  /* 大文字 */
+            LOWER   /* 小文字 */
+        };
+
+        // 呼び出す関数戻り値
+        public enum E_FUNCTION_RESULT
+        {
+            SUCCESS,/* 処理成功 */
+            FAILE   /* 処理失敗 */
+        };
+    }
+}
---
+++ 店舗マネージメントアシストシステム/ComputeHash/Program.cs
@@ -0,0 +1,32 @@
+﻿using static HashTool.HashHelper;
+using static ClassLibrary.Enum;
+using System;
+
+namespace ComputeHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            E_FUNCTION_RESULT ret_fuction;
+            string hash_value;
+
+            do
+            {
+                ret_fuction = String_Hash_MD5_32("abc", E_HASH_PARAMETER.UPPER, out hash_value);
+                if(E_FUNCTION_RESULT.SUCCESS != ret_fuction)
+                {
+                    break;
+                }
+                Console.WriteLine("MD5_32(Upper): {0}", hash_value);
+
+                ret_fuction = String_Hash_MD5_32("abc", E_HASH_PARAMETER.LOWER, out hash_value);
+                if(E_FUNCTION_RESULT.SUCCESS != ret_fuction)
+                {
+                    break;
+                }
+                Console.WriteLine("MD5_32(Upper): {0}", hash_value);
+            } while(false);
+        }
+    }
+}
